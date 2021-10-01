bcebc8b1f80d67cd442d75556424fe7b2064b731
John Shields <G00348436@gmit.ie>
2021-10-01 14:06:15 +0100

    Material updates to player and dev screenshots added.

--- PostTenebrasLux/Assets/Game/Scripts/PlayerProfiler.cs
+++ PostTenebrasLux/Assets/Game/Scripts/PlayerProfiler.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,9 +7,9 @@ namespace Game.Scripts
     {
         public LayerMask clickable;
         private NavMeshAgent _agent;
-        private Camera _camera;
 
         private Animator _animator;
+        private Camera _camera;
         private int _walkActive;
 
         private void Start()
@@ -21,14 +20,14 @@ namespace Game.Scripts
             _walkActive = Animator.StringToHash("WalkActive");
             _animator.SetBool(_walkActive, true);
         }
-        
+
         private void Update()
         {
             ClickToMove();
             AnimationState();
         }
-        
-        // Player movement
+
+        // Player movement - User 'ClickToMove' system.
         private void ClickToMove()
         {
             // if user input is left click on location the player (agent) will move to that location. 
@@ -38,25 +37,17 @@ namespace Game.Scripts
                 var ray = _camera.ScreenPointToRay(Input.mousePosition);
 
                 // allow user to cast a ray of where the player can move in the clickable areas (Layer Mask).
-                if (Physics.Raycast(ray, out var hitInfo, 100, clickable))
-                {
-                    _agent.SetDestination(hitInfo.point);
-                }
+                if (Physics.Raycast(ray, out var hitInfo, 100, clickable)) _agent.SetDestination(hitInfo.point);
             }
         }
 
-        // Player animations
+        // Player animations - Transitions player's animations based on movement.
         private void AnimationState()
         {
             // if player (agent) is moving set animation state to walk else revert to idle. 
             if (_agent.velocity != Vector3.zero)
-            {
                 _animator.SetBool(_walkActive, true);
-            }
-            else if (_agent.velocity == Vector3.zero)
-            {
-                _animator.SetBool(_walkActive, false); 
-            }
+            else if (_agent.velocity == Vector3.zero) _animator.SetBool(_walkActive, false);
         }
     }
 }
